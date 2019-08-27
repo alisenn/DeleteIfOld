21b42f433f56235788582905e9d50de0bafddd66
unknown <alisen4099@gmail.com>
2019-08-27 13:40:59 +0300

    my project

---
+++ DeleteIfOld/Delete.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Configuration;
+namespace DeleteIfOld
+{
+    class Delete
+    {
+        public void ControlAndDelete()
+        {
+            var path = ConfigurationManager.AppSettings["FilePath"];
+            var Year = ConfigurationManager.AppSettings["Year"];
+            int i = 0;
+
+            if (!(Directory.Exists(path)))
+            {
+                Console.WriteLine("Bu isimde bir klasör yok");
+                return;
+            }
+
+            if (!(int.TryParse(Year,out i)))
+            {
+                Console.WriteLine("Sayı doğru formtta girilmedi");
+                return;
+            }
+            int y = Int32.Parse(Year);
+            var now = DateTime.Now;
+            string[] files = Directory.GetFiles(path);
+
+            foreach (string fileName in files)
+            {
+                DateTime lastWriteTime = File.GetLastWriteTime(fileName);
+                DateTime FewYears = lastWriteTime.AddYears(y);
+                if (FewYears < now)
+                {
+                    File.Delete(fileName);
+                }
+            }
+           
+        }
+    }
+}
---
+++ DeleteIfOld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DeleteIfOld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var del = new Delete();
+            del.ControlAndDelete();
+        }
+    }
+}
